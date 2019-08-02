ae2a2ffbcf3763d6cc61e2afb9c586cc3b40e1b0
FiHammer <lualbi11@aol.de>
2019-08-02 14:00:16 +0200

    phh

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,49 @@
+import org.Bsp.Set_List_Map.MapBsp;
 import org.Test.Test.Person;
 import org.Test.Test.Raum;
+import java.util.Random;
 
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        MapBsp mp = new MapBsp();
+        mp.doWork();
+
+        /*
+
+            1   - 10
+            min - max
+
+            1. zwischen min & max => 2-9
+            int inclMin = 0;
+            r.nextInt(max-min+inclMin)+min+1-inclMin;
+
+            2. von min (incl) & max
+            r.nextInt(max-min)+min
 
 
+         */
+
+        /*
+
+        Random rand = new Random();
+
+        int inclMin = 0;
+        int inclMax = 1;
+        int max = 5;
+        int min = 2;
+        if (inclMax == 0) {
+            max -= 1;
+        }
+        if (inclMin == 0) {
+            min += 1;
+        }
+
+        for (int x=0; x<10; x++) {
+            System.out.println(rand.nextInt(max-min+1)+min);
+        }
+        */
         System.out.println("END!");
     }
 }
--- src/org/Bsp/Set_List_Map/MapBsp.java
+++ src/org/Bsp/Set_List_Map/MapBsp.java
@@ -1,19 +1,34 @@
 package org.Bsp.Set_List_Map;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 public class MapBsp {
-    private Map<String, String> map_hash;
-
+    private HashMap<ArrayList<Integer>, ArrayList<String>> hm;
+    private ArrayList<Integer> arrInt;
     public MapBsp() {
         // init the Map
 
-        map_hash = new HashMap<>();
-        map_hash.put("Hi", "MY DATA");
+        hm = new HashMap<>();
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("Date 1");
+        arrInt = new ArrayList<>();
+        arrInt.add(7);
 
+        hm.put(arrInt, new ArrayList<>());
+        hm.put(new ArrayList<Integer>(4), arr);
 
+        /*
 
+        {
+        "string": [1,2,"h",5]
+        }
 
+        */
+
+    }
+
+    public void doWork() {
+        System.out.println(hm.get(arrInt));
     }
 }
