ac1dbdbbaed7b2305b29ed0cdb99c0da4434e4b3
FiHammer <lualbi11@aol.de>
2019-08-26 10:59:16 +0200

    Wordprocessor

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
-import org.Unterricht.Uebung.Banken.Bank;
-import org.Unterricht.Uebung.Banken.Testklasse;
-import org.Unterricht.Uebung.SamBank.Test;
-
+import org.Unterricht.Uebung.Wordprocessor.Wordprocessor;
 
 public class Main {
     public static void main(String[] args) {
 
         //new Testklasse();
-
-        Test t = new Test();
-
+        System.out.println(Wordprocessor.process("Hallo _Ich bin TOLL_"));
+        Wordprocessor.simulate();
+        Wordprocessor.simulate();
 
 
         System.out.println("END!");
