85e19e958d4fd4de9a69f94ba8c3e2a6b06f96fd
FiHammer <lualbi11@aol.de>
2019-09-13 17:39:34 +0200

    ok something changed

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 //import org.BlueJ.Kapitel05.Baelle.BallDemo;
-import org.Unterricht.Uebung.SamBaelle.BallDemo;
+//import org.Unterricht.Uebung.SamBaelle.BallDemo;
+import org.Unterricht.Uebung.Baelle.BallDemo;
 
 public class Main {
     public static void main(String[] args) {
         //new Testklasse();
         BallDemo bd = new BallDemo();
-        bd.springenInDerSchachtel(5);
+        bd.springenInDerSchachtel(1);
         //bd.x();
         System.out.println("END!");
     }
